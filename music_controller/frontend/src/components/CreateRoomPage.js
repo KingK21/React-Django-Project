08523f6fb1dbc7991287d0974ec71382487ef599
KingK21 <jionatil@gmail.com>
2021-06-27 19:07:17 -0400

    Wrapping up component routes

---
+++ music_controller/frontend/src/components/CreateRoomPage.js
@@ -0,0 +1,11 @@
+import Reaat, { Component } from "react";
+
+export default class CreateRoomPage extends Component {
+    constructor(props){
+        super(props);
+    }
+
+    render() {
+        return <p>This is the create room page</p>;
+    }
+}
---
+++ music_controller/frontend/src/components/RoomJoinPage.js
@@ -0,0 +1,11 @@
+import Reaat, { Component } from "react";
+
+export default class RoomJoinPage extends Component {
+    constructor(props){
+        super(props);
+    }
+
+    render() {
+        return <p>This is the room join page</p>;
+    }
+}
